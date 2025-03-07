de5b02e16f3afee6a32115f544d8a9899c0250ba
AbdullahAliZulfik <tungau699@gmail.com>
2025-03-07 09:09:52 +0700

    baru

--- src/Tugas1.java
+++ src/Tugas1.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
-/* API */
+
 public class Tugas1 {
     public static void main(String[] args) {
-        /* Method */
+        
         Scanner deteksi = new Scanner(System.in);
         int pilihan;
         String userAd,userP,userAdd,userPp;
-        /* Deklarasi Variabel */
+        
         
         System.out.print("Pilih Login:\n");
         System.out.print("1. Admin\n");
         System.out.print("2. Mahasiswa\n"); 
         System.out.print("Masukkan Pilihan:");
-        /* Output Pilihan Yang Akan Dipilih */
+        
 
         pilihan = deteksi.nextInt();
         deteksi.nextLine();
-        /* Untuk Melakukan Proses Input ,Kemudian agar sesudah type data integer ada space */
+        
 
         switch (pilihan) {
-            /* Switch berdasarkan pilihan */
+            
             case 1:
             System.out.print("Masukkan Username:");
             userAd = deteksi.nextLine();
@@ -30,13 +30,14 @@ public class Tugas1 {
 
             }else{
                 System.out.format("Login Gagal!Username Atau Password salah!");
-                /* 2 Kondisi, kondisi pertama apabila benar kondisi kedua apabila salah  */
+                
             }
 
 
                 
                 break;
-                /* menghentikan switch */
+                
+        
             case 2:
             System.out.print("Masukkan Nama:");
             userAdd = deteksi.nextLine();
